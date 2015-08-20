b237c260395070bd50a59cb192836e68d3994ffb
wzyxidian <wzyxidian@gmail.com>
2015-08-20 09:02:36 +0800

    project init

---
+++ src/cn/edu/xidian/sse/datatype/BasicDataType.java
@@ -0,0 +1,13 @@
+package cn.edu.xidian.sse.datatype;
+
+public class BasicDataType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
