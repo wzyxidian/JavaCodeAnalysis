77815308f79808a9780bb8d8d4587f7667f7a332
wzyxidian <wzyxidian@gmail.com>
2015-08-30 10:11:06 +0800

    new File FloatTest.java

---
+++ src/cn/edu/xidian/sse/datatype/FloatTest.java
@@ -0,0 +1,27 @@
+package cn.edu.xidian.sse.datatype;
+
+public class FloatTest {
+
+	/**
+	 * @author zhiyong wang
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("******Float的常量******\r\n");
+		System.out.println("Float.POSITIVE_INFINITY的值是：" + Float.POSITIVE_INFINITY);
+		System.out.println("Float.NEGATIVE_INFINITY的值是：" + Float.NEGATIVE_INFINITY);
+		System.out.println("Float.NaN的值是：" + Float.NaN);
+		System.out.println("Float.MAX_VALUE的值是：" + Float.MAX_VALUE);
+		System.out.println("Float.MIN_NORMAL的值是：" + Float.MIN_NORMAL);
+		System.out.println("Float.MIN_VALUE的值是：" + Float.MIN_VALUE);
+		System.out.println("Float.MAX_EXPONENT的值是：" + Float.MAX_EXPONENT);
+		System.out.println("Float.MIN_EXPONENT的值是：" + Float.MIN_EXPONENT);
+		System.out.println("Float.SIZE的值是：" + Float.SIZE);
+		System.out.println("Float.TYPE的值是：" + Float.TYPE);
+		
+		System.out.println("\r\n******Float的方法******");
+		
+		
+	}
+
+}
