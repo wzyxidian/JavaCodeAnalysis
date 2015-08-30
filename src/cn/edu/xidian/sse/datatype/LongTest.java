78ec31836ae1f3f8ea8958ccb9d44f9cf55b0c0d
wzyxidian <wzyxidian@gmail.com>
2015-08-30 09:00:07 +0800

    add ShortTest.java LongTest.java

--- src/cn/edu/xidian/sse/datatype/LongTest.java
+++ src/cn/edu/xidian/sse/datatype/LongTest.java
@@ -3,11 +3,12 @@ package cn.edu.xidian.sse.datatype;
 public class LongTest {
 
 	/**
-	 * @param args
+	 * @author zhiyong wang
+	 * @exception 这个跟IntegerTest相似，几乎所有的方法都是一样的
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/cn/edu/xidian/sse/datatype/ShortTest.java
+++ src/cn/edu/xidian/sse/datatype/ShortTest.java
@@ -3,11 +3,12 @@ package cn.edu.xidian.sse.datatype;
 public class ShortTest {
 
 	/**
-	 * @param args
+	 * @author zhiyong wang
+	 * @exception 这个可以直接参考IntegerTest,只有少许不同，其他都相同
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
